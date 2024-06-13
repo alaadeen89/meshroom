54f84056c30bbe0294dd329509df14baa99bd8b1
Fabien Castan <fabcastan@gmail.com>
2024-06-14 00:23:14 +0200

    [core] node: do not ask the node to upgrade internalAttributes
    internalAttributes are managed on the Meshroom side, not at the node
    level. So there is no reason to call the upgrade of internalAttributes
    on the node description.

--- meshroom/core/node.py
+++ meshroom/core/node.py
@@ -1633,13 +1633,8 @@ class CompatibilityNode(BaseNode):
 
         node.upgradeAttributeValues(upgradedAttrValues)
 
-        try:
-            upgradedIntAttrValues = node.nodeDesc.upgradeAttributeValues(intAttrValues, self.version)
-        except Exception as e:
-            logging.error("Error in the upgrade implementation of the node: {}.\n{}".format(self.name, str(e)))
-            upgradedIntAttrValues = intAttrValues
+        node.upgradeInternalAttributeValues(intAttrValues)
 
-        node.upgradeInternalAttributeValues(upgradedIntAttrValues)
         return node
 
     compatibilityIssue = Property(int, lambda self: self.issue.value, constant=True)
